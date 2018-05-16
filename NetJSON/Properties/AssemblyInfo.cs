ebd005e0e5c9569d22ce4e7389c75983795f364a
rpgmaker <olamide.bakre@gmail.com>
2018-05-15 22:50:51 -0400

    - Updated version to 1.2.5
    Signed-off-by: rpgmaker <olamide.bakre@gmail.com>

--- NetJSON/Properties/AssemblyInfo.cs
+++ NetJSON/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.4")]
-[assembly: AssemblyFileVersion("1.2.4")]
+[assembly: AssemblyVersion("1.2.5")]
+[assembly: AssemblyFileVersion("1.2.5")]
 #if !NET_35
 [assembly: SecurityRules(SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
 #endif
