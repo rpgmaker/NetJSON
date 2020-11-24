161c22d09c34fe4c1ec61753465c9751b577f0f4
rpgmaker <olamide.bakre@gmail.com>
2020-11-24 11:21:40 -0800

    Added unit tests and fixed .net 5 issue

---
+++ NetJSON.Net5_0.Tests/NetJSONTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NetJSON.Net5_0.Tests
+{
+    public class NetJSONTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var jsonString = "{\"All\":{\"ym\":\"ss\"},\"ervicesAuto\":{\"ym\":\"ss\"},\"ocessor\":{\"\":\"ss\"},\"sonalDevice\":{\"\":\"ss\"},\"rryCan\":{\"\":\"ss\"},\"licom\":{\"ymr\":\"ss\"}}";
+            var result = NetJSON.Deserialize<Dictionary<string, Dictionary<string, string>>>(jsonString);
+            Assert.True(result.ContainsKey("All") && result["All"].ContainsKey("ym") && result["All"]["ym"].Equals("ss"));
+        }
+    }
+}
